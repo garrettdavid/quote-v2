05b75fbd073da3d3747b98c70646ca3cb1f63b81
Garrett David <garrett_david@hotmail.com>
2019-06-16 20:39:41 -0700

    finish program and initial design

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './normalize.css';
+import './main.css';
+
+function Quote(props) {
+    return (
+        <div id="full-quote">
+            <p id="text">{props.text}</p>
+            <p id="author">{props.author}</p>
+        </div>
+    );
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: null,
+            author: null
+        };
+
+        this.handleQuoteClick = this.handleQuoteClick.bind(this);
+    }
+
+    // Quote API thanks to https://github.com/lukePeavey/quota
+    handleQuoteClick() {
+        fetch('https://quota.glitch.me/random')
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+              text: "\"" + data.quoteText + "\"",
+              author: '-' + data.quoteAuthor
+          });
+        });
+    }
+
+    componentDidMount() {
+        this.handleQuoteClick();
+    }
+
+    makeLink(text, author) {
+        return "twitter.com/intent/tweet?text=" + text + " -" + author;
+    }
+
+    render() {
+        return (
+            <div id="quote-box">
+                <Quote text={this.state.text} author={this.state.author} />
+                <a href={this.makeLink(this.state.text, this.state.author)} id="tweet-quote" class="button">Tweet Quote</a>
+                <a href="#quote-box" id="new-quote" onClick={this.handleQuoteClick} class="button">New Quote</a>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
