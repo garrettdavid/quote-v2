9ed5c6381cb65d8cec62e00a89ab7620d8261ebf
Garrett David <garrett_david@hotmail.com>
2019-08-04 15:21:41 -0700

    Updates

---
+++ precache-manifest.323ed7a93830c8733b0dcaa712bb8eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2995c266e390dc253e34ec3986c3fec4",
+    "url": "/quote-v2/index.html"
+  },
+  {
+    "revision": "3472ae4aafb8067656cc",
+    "url": "/quote-v2/static/css/main.39968881.chunk.css"
+  },
+  {
+    "revision": "b42aa7dd048f4b5d252a",
+    "url": "/quote-v2/static/js/2.5c4e487f.chunk.js"
+  },
+  {
+    "revision": "3472ae4aafb8067656cc",
+    "url": "/quote-v2/static/js/main.7d672f3c.chunk.js"
+  },
+  {
+    "revision": "9207501690b63a44e7bd",
+    "url": "/quote-v2/static/js/runtime~main.36907b90.js"
+  }
+]);
